71d4be7bae8d6905315e707e126c9cb8847f3f3c
Igor <igor3dvis@gmail.com>
2022-05-25 11:29:12 +0200

    state -> store (made 'smart-object' with metods)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import state, { addNewPost, updateTextPost, addNewMessage, updateTextMessage, subscride } from "./myRedux/state.js";
+import store from "./myRedux/state.js";
 import { unicId } from "./utils/idFromDateTime";
 
 const reRender = (state) => {
@@ -10,18 +10,18 @@ const reRender = (state) => {
     <React.StrictMode>
       <App
         state={state}
-        addNewPost={addNewPost}
-        updateTextPost={updateTextPost}
-        addNewMessage={addNewMessage}
-        updateTextMessage={updateTextMessage}
+        addNewPost={store.addNewPost.bind(store)}
+        updateTextPost={store.updateTextPost.bind(store)}
+        addNewMessage={store.addNewMessage.bind(store)}
+        updateTextMessage={store.updateTextMessage.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
 
-reRender(state);
+reRender(store.getState());
 
-subscride(reRender);
+store.subscride(reRender);
 
 console.log(unicId());
--- src/myRedux/state.js
+++ src/myRedux/state.js
@@ -1,7 +1,76 @@
-let reRender = () => {
-  console.log("REREDNDER");
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, text: "Hello, Word" },
+        { id: 2, text: "Post #2 alredy here!!!" },
+      ],
+      postTextDefault: "text new post ...",
+    },
+
+    messagesPage: {
+      dialogsData: [
+        { id: "1", name: "Oxana" },
+        { id: "2", name: "Ivan" },
+        { id: "3", name: "Patric" },
+        { id: "4", name: "Rita" },
+      ],
+      messagesData: [
+        { messageText: "How are You?" },
+        { messageText: "Hello, I am fine. Are You?" },
+        { messageText: "Ababahalamaga" },
+        { messageText: " I felt happy for I had passed exam!" },
+      ],
+      messageTextDefault: "enter some text ...",
+    },
+  },
+
+  getState() {
+    return this._state;
+  },
+
+  _callSubscriber  ()  {
+    console.log("REREDNDER");
+  },
+
+  addNewPost() {
+    let newPost = { id: 6, text: this._state.profilePage.postTextDefault };
+
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.postTextDefault = "";
+    this._callSubscriber(this._state);
+  },
+
+  updateTextPost(newText) {
+    this._state.profilePage.postTextDefault = newText;
+    this._callSubscriber(this._state);
+  },
+
+  addNewMessage() {
+    let newMessage = {
+      messageText: this._state.messagesPage.messageTextDefault,
+    };
+
+    this._state.messagesPage.messagesData.push(newMessage);
+    this._state.messagesPage.messageTextDefault = "";
+    this._callSubscriber(this._state);
+  },
+
+  updateTextMessage(newText) {
+    this._state.messagesPage.messageTextDefault = newText;
+    this._callSubscriber(this._state);
+  },
+
+  subscride(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
+
+
+export default store;
+
+/*
 let state = {
   profilePage: {
     postsData: [
@@ -60,5 +129,4 @@ export const updateTextMessage = (newText) => {
 export const subscride = (observer) => {
   reRender = observer;
 };
-
-export default state;
+*/
