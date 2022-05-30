4a3fb95ded364e65753c52ba7b8813978e2144ae
Igor <igor3dvis@gmail.com>
2022-05-30 20:26:04 +0200

    commit before REDUX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import store from "./myRedux/state.js";
+import store from "./myRedux/store.js";
 import { unicId } from "./utils/idFromDateTime";
 
 const reRender = (state) => {
